1bc03d4217210c8cbbf8f9c882026dcb8d608531
Samantha <contact@samanthanguyen.me>
2023-10-31 23:35:38 -0500

    chore(stylelint): properly register stylelint-order plugin
    Fixes #55

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -7,6 +7,7 @@ module.exports = {
 	plugins: [
 		'stylelint-declaration-block-no-ignored-properties',
 		'stylelint-high-performance-animation',
+		'stylelint-order',
 	],
 	rules: {
 		// --- Start: Core stylelint + stylistic rules ---
